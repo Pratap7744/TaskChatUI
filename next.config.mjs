c41eaaf7af41cd729950d0c81f7e0a0bf7729dc4
pratap <pratappawar8698567744@gmail.com>
2025-02-11 11:40:38 +0530

    LoginUI

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import "dotenv/config";
 
-export default nextConfig;
+console.log("NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+console.log("CLERK_SECRET_KEY:", process.env.CLERK_SECRET_KEY);
+const nextConfig = {
+    env: {
+      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+      CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    },
+  };
+  
+  export default nextConfig;
+  
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 
-import ChatInterface from "./chatUI";
-
+import RegistrationForm from "./Login";
 export default function Home() {
   return (
     <div>
-     <ChatInterface/>
+     <RegistrationForm/>
+  
     </div>
   );
 }
